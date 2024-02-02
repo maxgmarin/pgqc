1ddd55b3bb41e15b85a73f3af61222a9909b3c59
Max <mgmarin@g.harvard.edu>
2024-02-02 16:11:28 -0500

    Update to py requirements

--- pgqc/_version.py
+++ pgqc/_version.py
@@ -1,4 +1,8 @@
-from importlib.metadata import PackageNotFoundError, version
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    # for python v3.7 or earlier
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
 
 try:
     __version__ = version("pgqc")
